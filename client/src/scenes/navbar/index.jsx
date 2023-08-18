a498b78172dc966069d8f1854d9d8e4510fc8921
Jake Isaacs <jakehisaacs@gmail.com>
2023-08-17 22:42:07 -0400

    Setup login and register forms

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
     const primaryLight = theme.palette.primary.light;
     const alt = theme.palette.background.alt;
 
-    const fullName = `${user.firstName} ${user.lastName}`;
+    const fullName = user ? `${user.firstName} ${user.lastName}` : 'Joe Shmoe';
 
     return <FlexBetween padding='1rem 6%' backgroundColor={alt}>
         <FlexBetween gap='1.75rem'>
