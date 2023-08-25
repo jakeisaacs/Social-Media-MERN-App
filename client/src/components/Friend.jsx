d5d139e21b8906acd3626cfd2d5a6962b0bb2152
Jake Isaacs <jakehisaacs@gmail.com>
2023-08-25 18:22:51 -0400

    Added fake ads and friends list...

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -19,6 +19,8 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
     const main = palette.neutral.main;
     const medium = palette.neutral.medium;
 
+    console.log(friends);
+
     const isFriend = friends.find((friend) => friend._id === friendId);
 
     const patchFriend = async () => {
